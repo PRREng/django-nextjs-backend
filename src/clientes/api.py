8e6ac78cddb3f86a4613a91e6fdf9824dd1c9dbe
Pedro Rocha Ribeiro <pr.rocha70@gmail.com>
2024-07-09 12:34:57 -0300

    Now clientes needs jwtauth to be accessed

--- src/clientes/api.py
+++ src/clientes/api.py
@@ -1,18 +1,20 @@
 from typing import List
 from ninja import Router
 
+from ninja_jwt.authentication import JWTAuth
 from django.shortcuts import get_object_or_404
 from .models import Cliente
 from .schemas import ClienteListSchema, ClienteDetailSchema
 
 router = Router()
 
-@router.get("", response=List[ClienteListSchema])
+@router.get("", response=List[ClienteListSchema], 
+            auth=JWTAuth())
 def list_clientes(request):
     qs = Cliente.objects.all()
     return qs
 
-@router.get("{client_id}/", response=ClienteDetailSchema)
+@router.get("{client_id}/", response=ClienteDetailSchema, auth=JWTAuth)
 def get_cliente(request, client_id:str):
     obj = get_object_or_404(Cliente, id=client_id)
     return obj
