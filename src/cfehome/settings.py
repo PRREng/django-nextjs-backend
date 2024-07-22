758f653547956a0690968ac20645ad0b4c2591a5
Pedro Rocha Ribeiro <pr.rocha70@gmail.com>
2024-07-22 10:54:16 -0300

    updated csrf trusted origins

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -17,7 +17,12 @@ SECRET_KEY = config("DJANGO_SECRET_KEY", cast=str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", cast=bool, default=False)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://*.vercel.app",
+    "https://*.vercel.app",
+]
 
 
 # Application definition
